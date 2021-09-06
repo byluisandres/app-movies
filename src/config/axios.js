50e57456c02b45bd27e9cc690f8f9f86d84a277e
Luis andrés bolanos <luisandres@bolanos>
2021-09-06 20:17:16 +0200

    Hecho el docs para github page

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
-
+const config = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'text/plain',
+  },
+}
 const clienteAxios = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
+  https: config,
   params: {
     api_key: '4e5d0990973bb9d400910a2cc2c20688',
     language: 'es-ES',
